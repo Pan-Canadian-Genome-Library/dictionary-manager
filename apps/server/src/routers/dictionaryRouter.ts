b71acf870aedfb1e069a930551dd340e6d5d495a
James Lopez <jamestlopez.code@gmail.com>
2025-09-11 10:12:39 -0400

    fix(auth): set up wrapasync

--- apps/server/src/routers/dictionaryRouter.ts
+++ apps/server/src/routers/dictionaryRouter.ts
@@ -19,7 +19,6 @@
 import { Router } from 'express';
 import * as dictionaryController from '../controllers/dictionaryController';
 import { wrapAsync, wrapAsyncAuth } from './wrappers';
-import { authAdminMiddleware } from '../middleware/auth';
 
 const router = Router();
 
@@ -31,7 +30,7 @@ router.get('', wrapAsync(dictionaryController.listDictionaries));
 /**
  * Create Dictionary
  */
-router.post('', authAdminMiddleware(), wrapAsyncAuth(dictionaryController.createDictionary));
+router.post('', wrapAsyncAuth(dictionaryController.createDictionary));
 
 /**
  * Get Dictionary by ID
@@ -41,12 +40,12 @@ router.get('/:dictId', wrapAsync(dictionaryController.getDictionary));
 /**
  * Add Schema to Dictionary
  */
-router.post('/:dictId/schemas', authAdminMiddleware(), wrapAsyncAuth(dictionaryController.addSchema));
+router.post('/:dictId/schemas', wrapAsyncAuth(dictionaryController.addSchema));
 
 /**
  * Update Schema for Dictionary
  */
-router.put('/:dictId/schemas', authAdminMiddleware(), wrapAsyncAuth(dictionaryController.updateSchema));
+router.put('/:dictId/schemas', wrapAsyncAuth(dictionaryController.updateSchema));
 
 /**
  * Get Schema from Dictionary
